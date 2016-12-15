1bd1c194362d86983e2e0a7aa06680edb43f1255
Rus Daniel <rusdaniel1989@gmail.com>
2016-12-15 17:16:36 +0200

    new files

---
+++ Block/Training.php
@@ -0,0 +1,10 @@
+<?php
+namespace Magento\Training\Block;
+
+class Training extends \Magento\Framework\View\Element\Template
+{
+    public function getText()
+    {
+        return 'This is a test';
+    }
+}
